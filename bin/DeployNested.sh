024f504bd3c032b4f06c3a1fe6225647e78a95a5
Joe Sueper <joseph.sueper@gmail.com>
2017-07-23 21:35:07 -0600

    Latest Update

--- bin/DeployNested.sh
+++ bin/DeployNested.sh
@@ -5,6 +5,8 @@ set -o errexit -o xtrace
 bucket=quickstart-reference-as
 key=zabbixgrafananested/setup/latest
 
+serverspec_bucket=serverspec-test
+
 
 aws s3api create-bucket --bucket ${bucket} --region us-east-1 --acl public-read
 
@@ -12,6 +14,6 @@ aws s3 cp ../BootStrapScripts/ "s3://${bucket}/${key}/Scripts/" --recursive --ac
 
 aws s3 cp ../NestedTemplates/ "s3://${bucket}/${key}/" --recursive --acl public-read
 
-aws s3 cp ../ServerSpecTests/ "s3://serverspec-test/" --recursive --acl public-read
+aws s3 cp ../ServerSpecTests/ "s3://${serverspec_bucket}/" --recursive --acl public-read
 
 aws cloudformation create-stack --template-url https://s3.amazonaws.com/"${bucket}/${key}/RootTemplate"/RootNestedTemplate.template --stack-name ZG  --parameters file://paramsNested.json --disable-rollback --capabilities CAPABILITY_IAM
